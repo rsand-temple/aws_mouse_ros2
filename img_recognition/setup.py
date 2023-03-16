cab3dfd76d8d878fd3b9add803f0dd5fd221f986
Richard Sand <richard.sand0001@temple.edu>
2023-03-16 19:47:58 -0400

    img_recognition

--- img_recognition/setup.py
+++ img_recognition/setup.py
@@ -4,8 +4,8 @@ package_name = 'img_recognition'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version='0.0.1',
+    packages=[package_name,package_name + '/msg',package_name + '/param'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -16,7 +16,7 @@ setup(
     maintainer='rsand',
     maintainer_email='richard.sand0001@temple.edu',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='Apache2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
