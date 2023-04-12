546f57cd7a46ebf5c6f51673e229d1d37d47ae15
Richard Sand <richard.sand0001@temple.edu>
2023-04-12 14:46:02 -0400

    fixing launch files

--- jetbot_description/setup.py
+++ jetbot_description/setup.py
@@ -8,10 +8,14 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, glob('launch/*.launch'))
+        ('share/' + package_name + '/config/', glob('config/*')),
+        ('share/' + package_name + '/launch/', glob('launch/*')),
+        ('share/' + package_name + '/meshes/', glob('meshes/*')),
+        ('share/' + package_name + '/meshes-jetbot-ros/', glob('meshes-jetbot-ros/*')),
+        ('share/' + package_name + '/rviz/', glob('rviz/*')),
+        ('share/' + package_name + '/urdf/', glob('urdf/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
