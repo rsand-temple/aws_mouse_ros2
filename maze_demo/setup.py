e909acdb1569d35adbb848f2a8dcdf6f42fd1e49
Richard Sand <richard.sand0001@temple.edu>
2023-10-13 13:36:08 -0400

    explore_world

--- maze_demo/setup.py
+++ maze_demo/setup.py
@@ -1,36 +1,37 @@
 import os
 from setuptools import setup
 
-package_name = 'maze_world'
+package_name = 'maze_demo'
 
 #def package_files(directory):
 #    paths = []
 #    for (path, directories, filenames) in os.walk(directory):
 #        for filename in filenames:
-#            paths.append(os.path.join('.', path, filename))
+#            paths.append(os.path.join('..', path, filename))
 #    return paths
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=[package_name,package_name + '/mouse_common'],
 #    package_data={'': package_files('launch')},
+   
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name + '/launch/', ['launch/world.launch']),
+#        ('share/' + package_name, [package_files('launch')}),
+        ('share/' + package_name + '/launch/', ['launch/explore_world.launch']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rsand',
     maintainer_email='richard.sand0001@temple.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Maze Demo',
+    license='Apache2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'world.launch = maze_world.world.launch:main'
+            'maze_demo = maze_demo.maze_runner:main'
         ],
     },
 )
+
