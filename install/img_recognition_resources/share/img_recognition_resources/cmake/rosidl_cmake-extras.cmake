b19e3fcdea72513bea57acea147eece64f9415b5
Richard Sand <richard.sand0001@temple.edu>
2023-10-13 14:48:04 -0400

    logs

--- install/img_recognition_resources/share/img_recognition_resources/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(img_recognition_resources_IDL_FILES "msg/Inference.idl;msg/Prediction.idl;srv/PictureInterval.idl;srv/SaveAction.idl;srv/SelectLabel.idl")
-set(img_recognition_resources_INTERFACE_FILES "msg/Inference.msg;msg/Prediction.msg;srv/PictureInterval.srv;srv/PictureInterval_Request.msg;srv/PictureInterval_Response.msg;srv/SaveAction.srv;srv/SaveAction_Request.msg;srv/SaveAction_Response.msg;srv/SelectLabel.srv;srv/SelectLabel_Request.msg;srv/SelectLabel_Response.msg")
--- install/pid_tune/share/pid_tune/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(pid_tune_IDL_FILES "msg/PidTune.idl")
-set(pid_tune_INTERFACE_FILES "msg/PidTune.msg")
